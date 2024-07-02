046ef255248443dacd79a4944449ee9ef2d121f0
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-07-02 17:59:26 +0300

    Added custom hook.
    Persist state to local storage

---
+++ Shopping-Cart/src/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+export const useLocalStorage = (key, initialState) => {
+    const [persistedState, setPersistedState] = useState(() => {
+        let currentState = localStorage.getItem(key);
+        if (currentState) {
+            return JSON.parse(currentState);
+        } else {
+            return initialState;
+        }
+    });
+
+    const setStateLocalStorage = (value) => {
+        setPersistedState(value);
+        localStorage.setItem(key, value);
+    };
+    return [ persistedState, setStateLocalStorage ];
+};
