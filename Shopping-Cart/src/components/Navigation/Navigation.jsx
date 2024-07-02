8cf1d4de4148b41556e1fbc83ee27cfdfbc68b11
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-07-02 18:00:59 +0300

    Added shopping cart Link.
    Sync current page with Active nav link

--- Shopping-Cart/src/components/Navigation/Navigation.jsx
+++ Shopping-Cart/src/components/Navigation/Navigation.jsx
@@ -1,51 +1,67 @@
 import React from 'react';
+import { useState } from 'react'
+
+
+import { Link } from 'react-router-dom'
+
+
 import {
     HomeOutlined,
     ProductOutlined,
     ShoppingCartOutlined
 } from '@ant-design/icons';
 import { Menu } from 'antd';
+
+import { useLocalStorage } from '../../hooks/useLocalStorage'
 import styles from './navigation.module.css'
 const items = [
     {
         key: '1',
         icon: <HomeOutlined />,
-        label: 'Home',
+        label: (
+            <Link to="/">Home</Link>
+        ),
     },
 
 
     {
         key: '2',
         icon: <ProductOutlined />,
-        label: 'Products',
+        label: (
+            <Link to="products">Products</Link>
+
+        ),
     },
 
     {
         key: '3',
         icon: <ShoppingCartOutlined />,
-        label: 'Shopping Cart',
-
-
-
-
+        label: (
+            <Link to="/shopping-cart">Shopping Cart</Link>
+        )
 
     },
 
 ];
 export const Navigation = () => {
+    const [persistedPageKey, setStateLocalStorage] = useLocalStorage('currentPage', 1)
+    const persistKeyLocalStorage = (item) => {
+        const { key } = item
+        setStateLocalStorage(key)
+    }
     return (
         <>
             <nav className={styles.nav__wrapper}>
                 <Menu
                     style={{
                         itemBg: "f0000000",
-                        // height: windowHeight - 62,
                     }}
-                    defaultSelectedKeys={['1']}
+                    defaultSelectedKeys={[`${persistedPageKey}`]}
                     defaultOpenKeys={['sub1']}
                     theme='light'
                     items={items}
                     className={styles.nav}
+                    onClick={(item, key) => persistKeyLocalStorage(item, key)}
                 />
             </nav>
         </>
