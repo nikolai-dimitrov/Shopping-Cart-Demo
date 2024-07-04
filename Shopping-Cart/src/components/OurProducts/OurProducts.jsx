9e041fb44f7cfa6bc9033816cdeeab5b22131d4e
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-07-04 12:31:15 +0300

    Added product carousel.
    Customised antd carousel and initialized example product page with hard coded data.

--- Shopping-Cart/src/components/OurProducts/OurProducts.jsx
+++ Shopping-Cart/src/components/OurProducts/OurProducts.jsx
@@ -1,10 +1,19 @@
 import { Product } from './Product/Product'
+import styles from "./our-products.module.css"
 
 export const OurProducts = () => {
   return (
     <>
-      <h1>Products Page</h1>
-      <Product />
+      {/* <div id="our-products"> */}
+      <div className={styles.our__products}>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+
+      </div>
     </>
   )
 }
--- Shopping-Cart/src/components/OurProducts/Product/Product.jsx
+++ Shopping-Cart/src/components/OurProducts/Product/Product.jsx
@@ -1,28 +1,46 @@
+import styles from './product.module.css'
+// import iphone14 from '../../../../public/apple-2024-2-removebg-preview.jpg'
 import { Carousel } from 'antd';
 const contentStyle = {
-  margin: 0,
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
+    margin: 0,
+    height: '270px',
+    color: '#fff',
+    textAlign: 'center',
+    background: '#006ba1',
+
 };
 export const Product = () => (
-  <>
-    <Carousel arrows infinite={false}>
-      <div>
-        <h3 style={contentStyle}>1</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>2</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>3</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>4</h3>
-      </div>
-    </Carousel>
-    <br />
-  </>
+    <>
+        <Carousel arrows infinite={false} style={{
+            border: '1px solid #f5f5f5',
+        }}>
+            <article>
+                <div style={contentStyle}>
+                    <div className={styles.img__wrapper}>
+                        <img src="https://fdn.gsmarena.com/imgroot/static/headers/makers/apple-2024-2.jpg" alt="" className={styles.img} />
+                        {/* <img src={iphone14} alt="" className={styles.img} /> */}
+                    </div>
+                    <p className={styles.title}>Iphone 14 Pro Max</p>
+                    <p className={styles.price} >Price: <span>$</span>2000</p>
+                </div>
+
+            </article>
+            <article>
+                <div style={contentStyle}>
+                    <h2>Technical Specifications</h2>
+                    <div className={styles.info__wrapper}>
+                        <p>Capacity: <span>128BG</span></p>
+                        <p>Size: <span>13 inches</span></p>
+                        <p>Processor: <span>1.7GHz</span></p>
+                        <p>RAM: <span>4GB</span></p>
+                        <p>Battery: <span>Ionic 74mAh</span></p>
+                        <p>USB Type: <span>C</span></p>
+                        <p>Charger Included: <span>NO</span></p>
+                    </div>
+                    <button>Add to cart</button>
+                </div>
+            </article>
+        </Carousel>
+        <br />
+    </>
 )
