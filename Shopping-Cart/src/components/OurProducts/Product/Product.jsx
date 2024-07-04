541cfc3d990e73a1288cb6cb3955c1db250c47e9
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-07-04 20:30:28 +0300

    Removed hard coded data from cards.
    Added real data fetched from the server.

--- Shopping-Cart/src/components/OurProducts/Product/Product.jsx
+++ Shopping-Cart/src/components/OurProducts/Product/Product.jsx
@@ -1,5 +1,4 @@
 import styles from './product.module.css'
-// import iphone14 from '../../../../public/apple-2024-2-removebg-preview.jpg'
 import { Carousel } from 'antd';
 const contentStyle = {
     margin: 0,
@@ -9,7 +8,19 @@ const contentStyle = {
     background: '#006ba1',
 
 };
-export const Product = () => (
+export const Product = ({
+    _id,
+    item,
+    display,
+    processor,
+    ram,
+    imageUrl,
+    capacity,
+    battery,
+    usbType,
+    price,
+    chargerIncluded
+}) => (
     <>
         <Carousel arrows infinite={false} style={{
             border: '1px solid #f5f5f5',
@@ -17,11 +28,10 @@ export const Product = () => (
             <article>
                 <div style={contentStyle}>
                     <div className={styles.img__wrapper}>
-                        <img src="https://fdn.gsmarena.com/imgroot/static/headers/makers/apple-2024-2.jpg" alt="" className={styles.img} />
-                        {/* <img src={iphone14} alt="" className={styles.img} /> */}
+                        <img src={imageUrl} alt="" className={styles.img} />
                     </div>
-                    <p className={styles.title}>Iphone 14 Pro Max</p>
-                    <p className={styles.price} >Price: <span>$</span>2000</p>
+                    <p className={styles.title}>{item}</p>
+                    <p className={styles.price} >Price: <span>$</span>{price}</p>
                 </div>
 
             </article>
@@ -29,13 +39,13 @@ export const Product = () => (
                 <div style={contentStyle}>
                     <h2>Technical Specifications</h2>
                     <div className={styles.info__wrapper}>
-                        <p>Capacity: <span>128BG</span></p>
-                        <p>Size: <span>13 inches</span></p>
-                        <p>Processor: <span>1.7GHz</span></p>
-                        <p>RAM: <span>4GB</span></p>
-                        <p>Battery: <span>Ionic 74mAh</span></p>
-                        <p>USB Type: <span>C</span></p>
-                        <p>Charger Included: <span>NO</span></p>
+                        <p>Capacity: <span>{capacity}BG</span></p>
+                        <p>Size: <span>{display} inches</span></p>
+                        <p>Processor: <span>{processor}GHz</span></p>
+                        <p>RAM: <span>{ram}GB</span></p>
+                        <p>Battery: <span>{battery}mAh</span></p>
+                        <p>USB Type: <span>{usbType}</span></p>
+                        <p>Charger Included: <span>{chargerIncluded}</span></p>
                     </div>
                     <button>Add to cart</button>
                 </div>
