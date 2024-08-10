583a90832a78206fdb28a7cc5f60a9338edde7c0
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-10 13:06:33 +0300

    Changed css naming convention

--- Shopping-Cart/src/components/CardSkeleton/CardSkeleton.jsx
+++ Shopping-Cart/src/components/CardSkeleton/CardSkeleton.jsx
@@ -6,14 +6,14 @@ export const CardSkeleton = ({ cards }) => {
     return (
         Array(cards).fill(0).map((_, index) =>
             <article className={styles.card} key={index}>
-                <div className={styles.card__skeleton}>
-                    <div className={styles.img__wrapper}>
+                <div className={styles.cardSkeleton}>
+                    <div className={styles.imgWrapper}>
                         <Skeleton className={styles.img} baseColor={'#ebece7'} />
                     </div>
                     <Skeleton className={styles.title} />
 
                     <Skeleton className={styles.price} />
-                    <div className={styles.btn__wrapper}>
+                    <div className={styles.btnWrapper}>
                         <Skeleton width={'40px'} />
                         <Skeleton width={'40px'} />
                     </div>
@@ -24,4 +24,4 @@ export const CardSkeleton = ({ cards }) => {
         )
 
     )
-}
+} 
