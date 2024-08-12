f10096659f0ee679082a8870f6f7afbe7a813cbb
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-12 16:24:28 +0300

    Added empty cart layout.

--- Shopping-Cart/src/components/ShoppingCart/ShoppingCart.jsx
+++ Shopping-Cart/src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,14 +2,23 @@ import { useContext } from 'react'
 import { ProductContext } from '../../contexts/ProductContext'
 
 import { Reveal } from '../Reveal/Reveal';
+import {
 
+    ShoppingCartOutlined
+} from '@ant-design/icons';
 import styles from './shopping-cart.module.css'
 export const ShoppingCart = () => {
     const { testValue } = useContext(ProductContext)
     return (
-        <>
-            <p>{testValue}</p>
-            <div>Shopping Cart</div>
-        </>
+        <section className={styles.shoppingCart}>
+
+            <div className={styles.iconContainer}>
+                <div className={styles.innerIconContainer}>
+                    <h1 className={styles.heading}>Your cart is empty.</h1>
+                    <ShoppingCartOutlined className={styles.icon} />
+                </div>
+            </div>
+
+        </section>
     )
 }
