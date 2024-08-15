66df14195246aee8953249729f6ff79441e50ab8
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-15 18:36:26 +0300

    Sync product context with local storage
    Persist items added in cart between refresh.

--- Shopping-Cart/src/contexts/ProductContext.jsx
+++ Shopping-Cart/src/contexts/ProductContext.jsx
@@ -1,22 +1,38 @@
 import { createContext, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { useLocalStorage } from '../hooks/useLocalStorage';
+
 export const ProductContext = createContext();
 
 export const ProductProvider = ({ children }) => {
-    const [cartProducts, setCartProducts] = useState([]);
+    const [cartProducts, setCartProducts] = useLocalStorage('cartProducts', []);
+
 
     const addToCartHandler = (currentProduct) => {
-        setCartProducts(cartProducts => [...cartProducts, currentProduct])
+        setCartProducts(currentProduct, 'add');
+
     }
+
+    const removeFromCartHandler = (currentProduct) => {
+        setCartProducts(currentProduct, 'remove');
+    }
+
 
     const testValue = 5
     const productContextValues = {
         addToCartHandler,
+        removeFromCartHandler,
+        cartProducts,
         testValue,
     }
 
+    // console.log(cartProducts, 'in context')
+    let stringifiedState = JSON.stringify([
+        ...cartProducts,
+    ]);
 
+    localStorage.setItem('cartProducts', stringifiedState);
     return (
         <ProductContext.Provider value={productContextValues}>
             {children}
--- Shopping-Cart/src/hooks/useLocalStorage.js
+++ Shopping-Cart/src/hooks/useLocalStorage.js
@@ -1,17 +1,26 @@
 import { useState } from "react";
 export const useLocalStorage = (key, initialState) => {
-    const [persistedState, setPersistedState] = useState(() => {
-        let currentState = localStorage.getItem(key);
-        if (currentState) {
-            return JSON.parse(currentState);
-        } else {
-            return initialState;
-        }
-    });
+	const [persistedState, setPersistedState] = useState(() => {
+		let currentState = localStorage.getItem(key);
+		if (currentState) {
+			return JSON.parse(currentState);
+		} else {
+			return initialState;
+		}
+	});
 
-    const setStateLocalStorage = (value) => {
-        setPersistedState(value);
-        localStorage.setItem(key, value);
-    };
-    return [ persistedState, setStateLocalStorage ];
+	const setStateLocalStorage = (currentProduct, operationString) => { 
+		if (operationString == "add") {
+			setPersistedState((persistedState) => [
+				...persistedState,
+				currentProduct,
+			]);
+		} else if (operationString == "remove") {
+			setPersistedState((persistedState) =>
+				persistedState.filter((item) => item._id != currentProduct._id)
+			);
+		}
+	};
+
+	return [persistedState, setStateLocalStorage];
 };
