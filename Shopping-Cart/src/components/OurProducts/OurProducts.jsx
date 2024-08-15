8c79bd75109fb28759dc5bad251b30819b6b8f4c
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-15 18:40:30 +0300

    File indentation changed

--- Shopping-Cart/src/components/OurProducts/OurProducts.jsx
+++ Shopping-Cart/src/components/OurProducts/OurProducts.jsx
@@ -10,67 +10,67 @@ import { Pagination, notification } from 'antd';
 import styles from "./our-products.module.css"
 
 export const OurProducts = () => {
-  const [electronics, setElectronics] = useState([]);
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(true);
-  const [api, contextHolder] = notification.useNotification();
+	const [electronics, setElectronics] = useState([]);
+	const [page, setPage] = useState(1);
+	const [isLoading, setIsLoading] = useState(true);
+	const [api, contextHolder] = notification.useNotification();
 
-  const { testValue } = useContext(ProductContext)
+	const { testValue } = useContext(ProductContext)
 
-  useEffect(() => {
-    fetch('http://localhost:3030/jsonstore/electronics')
-      .then(response => response.json())
-      .then(data => {
-        const nestedArrays = splitArrayToSubArrays(Object.values(data));
-        setElectronics(nestedArrays);
-        setIsLoading(false);
+	useEffect(() => {
+		fetch('http://localhost:3030/jsonstore/electronics')
+			.then(response => response.json())
+			.then(data => {
+				const nestedArrays = splitArrayToSubArrays(Object.values(data));
+				setElectronics(nestedArrays);
+				setIsLoading(false);
 
-      })
-      .catch(error => console.log(error.message));
-  }, []);
+			})
+			.catch(error => console.log(error.message));
+	}, []);
 
-  const openNotification = (pauseOnHover, productTitle) => {
-    api.success({
-      message: "Successful purchase!",
-      description: `You successfully added to cart ${productTitle}`,
-      showProgress: true,
-      pauseOnHover,
-    })
-  }
-  
-  const showNotification = (productTitle) => {
-    openNotification(true, productTitle)
-  }
+	const openNotification = (pauseOnHover, productTitle) => {
+		api.success({
+			message: "Successful purchase!",
+			description: `You successfully added to cart ${productTitle}`,
+			showProgress: true,
+			pauseOnHover,
+		})
+	}
 
-  let totalElectronicsCount = electronics.reduce(
-    (count, currentArr) => count + currentArr.length,
-    0
-  );
+	const showNotification = (productTitle) => {
+		openNotification(true, productTitle)
+	}
 
-  return (
+	let totalElectronicsCount = electronics.reduce(
+		(count, currentArr) => count + currentArr.length,
+		0
+	);
 
-    <>
+	return (
 
-      <div className={styles.ourProducts}>
-        <>
-          {contextHolder}
-        </>
-        {isLoading ? <CardSkeleton cards={9} /> : electronics[page - 1]?.map(item => (<Product key={item._id} {...item} showNotification={showNotification} />))}
-        <Pagination
-          simple={{
-            readOnly: true,
-          }}
-          defaultCurrent={1}
-          total={totalElectronicsCount}
-          current={page}
-          pageSize={9}
-          onChange={(page) => setPage(page)}
-          style={{
-            width: '93%',
-            justifyContent: 'center'
-          }}
-        />
-      </div>
-    </>
-  )
+		<>
+
+			<div className={styles.ourProducts}>
+				<>
+					{contextHolder}
+				</>
+				{isLoading ? <CardSkeleton cards={9} /> : electronics[page - 1]?.map(item => (<Product key={item._id} {...item} showNotification={showNotification} />))}
+				<Pagination
+					simple={{
+						readOnly: true,
+					}}
+					defaultCurrent={1}
+					total={totalElectronicsCount}
+					current={page}
+					pageSize={9}
+					onChange={(page) => setPage(page)}
+					style={{
+						width: '93%',
+						justifyContent: 'center'
+					}}
+				/>
+			</div>
+		</>
+	)
 }
