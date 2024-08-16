c47607d6b0b333310f88f6cd31e7306ca145d6bb
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-16 18:51:59 +0300

    Added shopping cart page basics
    Added basic layout for non-empty cart case.

--- Shopping-Cart/src/components/ShoppingCart/ShoppingCart.jsx
+++ Shopping-Cart/src/components/ShoppingCart/ShoppingCart.jsx
@@ -7,20 +7,32 @@ import {
 
     ShoppingCartOutlined
 } from '@ant-design/icons'
+
 import styles from './shopping-cart.module.css'
 export const ShoppingCart = () => {
     const { cartProducts, removeFromCartHandler } = useContext(ProductContext);
     return (
-        <section className={styles.shoppingCart}>
+        <section className={cartProducts.length > 0 ? `${styles.shoppingCart}` : `${styles.shoppingCart} ${styles.emptyCardLayout}`}>
             {cartProducts.length == 0 ? <div className={styles.iconContainer}>
                 <div className={styles.innerIconContainer}>
                     <h1 className={styles.heading}>Your cart is empty.</h1>
                     <ShoppingCartOutlined className={styles.icon} />
                 </div>
             </div> :
-                <div>
-                    {cartProducts.map(product => (<CartItem key={product._id} product={product} removeFromCartHandler={removeFromCartHandler} />))}
-                </div>
+                <>
+                    <h1 className={styles.heading}>Your Cart ({cartProducts.length} items)</h1>
+                    <div className={styles.table}>
+                        <ul className={styles.container}>
+                            <li>Item</li>
+                            <li>Price</li>
+                            <li>Quantity</li>
+                            <li>Total</li>
+                        </ul>
+                    </div>
+                    <div className={styles.productCards}>
+                        {cartProducts.map(product => (<CartItem key={product._id} product={product} removeFromCartHandler={removeFromCartHandler} />))}
+                    </div>
+                </>
             }
         </section>
     )
