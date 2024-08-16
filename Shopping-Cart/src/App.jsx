105aa1e8b062499c8dd9c6ab90f4df42c2dcf4b7
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-16 13:29:40 +0300

    Removed Context Outlet

--- Shopping-Cart/src/App.jsx
+++ Shopping-Cart/src/App.jsx
@@ -5,7 +5,7 @@ import { Navigation } from "./components/Navigation/Navigation";
 import { Home } from "./components/Home/Home";
 import { OurProducts } from "./components/OurProducts/OurProducts";
 import { ShoppingCart } from "./components/ShoppingCart/ShoppingCart";
-import { ProductContextOutlet } from './contexts/ProductContext'
+import { ProductProvider } from './contexts/ProductContext'
 
 import { Layout } from "antd";
 
@@ -16,20 +16,18 @@ function App() {
     return (
         <>
             <Header />
-            <div className={styles.layout}>
-                <Navigation />
-                <main id="main">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route element={<ProductContextOutlet />}>
-
+            <ProductProvider>
+                <div className={styles.layout}>
+                    <Navigation />
+                    <main id="main">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
                             <Route path="/products" element={<OurProducts />} />
                             <Route path="/shopping-cart" element={<ShoppingCart />} />
-                        </Route >
-
-                    </Routes>
-                </main>
-            </div>
+                        </Routes>
+                    </main>
+                </div>
+            </ProductProvider>
             <Footer className={styles.footer} />
 
 
--- Shopping-Cart/src/contexts/ProductContext.jsx
+++ Shopping-Cart/src/contexts/ProductContext.jsx
@@ -1,5 +1,4 @@
-import { createContext, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { createContext } from 'react';
 
 import { useLocalStorage } from '../hooks/useLocalStorage';
 
@@ -41,11 +40,3 @@ export const ProductProvider = ({ children }) => {
         </ProductContext.Provider>
     )
 }
-
-export const ProductContextOutlet = () => {
-    return (
-        <ProductProvider>
-            <Outlet />
-        </ProductProvider>
-    )
-};
