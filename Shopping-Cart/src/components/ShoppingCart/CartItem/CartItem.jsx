273f8f102d3cc2b78a86f538eaa4e468e46d0759
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-16 18:54:03 +0300

    Added shopping cart item HTML & CSS

--- Shopping-Cart/src/components/ShoppingCart/CartItem/CartItem.jsx
+++ Shopping-Cart/src/components/ShoppingCart/CartItem/CartItem.jsx
@@ -3,12 +3,38 @@ import styles from './cart-item.module.css';
 export const CartItem = ({ product, removeFromCartHandler }) => {
     return (
         <>
-            <div>
-                <p>{product.item}</p>
-                <button className={styles.testBtn} onClick={() => removeFromCartHandler(product)}>
-                    Remove
-                </button>
-            </div>
+            <article className={styles.card}>
+                <div className={styles.item}>
+                    <div className={styles.imgContainer}>
+                        <img className={styles.img} src={product.imageUrl} alt="" />
+                    </div>
+                    <div className={styles.description}>
+                        <h1 className={styles.heading}>{product.item}</h1>
+                        <h4>Capacity: {product.capacity}</h4>
+                        <h4>USB Type: {product.usbType}</h4>
+                        <p>Charger Included: {product.chargerIncluded}</p>
+                    </div>
+                </div>
+                <p className={styles.price}>${product.price}</p>
+
+                <div className={styles.quantityContainer}>
+                    <button className={styles.leftSide}>
+                        -
+                    </button>
+                    <p>1</p>
+                    <button className={styles.rightSide}>
+                        +
+                    </button>
+                </div>
+                <div className={styles.totalPriceContainer}>
+                    <span className={styles.totalPrice}>${product.price}</span>
+
+                    <button className={styles.btn} onClick={() => removeFromCartHandler(product)}>
+                        X
+                    </button>
+                </div>
+
+            </article>
         </>
     )
 
