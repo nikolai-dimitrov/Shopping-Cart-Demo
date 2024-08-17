71131d04c91a2886974e9f0ab2a3939d61f36779
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-17 18:53:33 +0300

    Added productQuantity state and calculation handler.

--- Shopping-Cart/src/components/ShoppingCart/ShoppingCart.jsx
+++ Shopping-Cart/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useState, useContext } from 'react'
 import { ProductContext } from '../../contexts/ProductContext'
 
 import { CartItem } from './CartItem/CartItem'
@@ -11,6 +11,28 @@ import {
 import styles from './shopping-cart.module.css'
 export const ShoppingCart = () => {
     const { cartProducts, removeFromCartHandler } = useContext(ProductContext);
+
+    // Create object with key:productId , initialValue: '1' for all products in the cart than pass it as initial state.
+    let quantityInitialState = Object.fromEntries(cartProducts.map((el) => ([el._id, '1'])))
+    const [quantity, setQuantity] = useState(quantityInitialState)
+
+    const productQuantityHandler = (productId, operation) => {
+        setQuantity((quantity) => {
+            let quantityNumber = Number(quantity[productId])
+            if (quantityNumber <= 1 && operation == 'decrement') {
+                quantityNumber += 1
+            }
+            operation == 'increment' ? quantityNumber += 1 : quantityNumber -= 1
+            return ({
+                ...quantity,
+                [productId]: quantityNumber,
+            })
+        })
+    }
+
+    let grandTotalPrice = 0
+    cartProducts.forEach((el) => grandTotalPrice += el.price * quantity[el._id])
+
     return (
         <section className={cartProducts.length > 0 ? `${styles.shoppingCart}` : `${styles.shoppingCart} ${styles.emptyCardLayout}`}>
             {cartProducts.length == 0 ? <div className={styles.iconContainer}>
@@ -30,8 +52,9 @@ export const ShoppingCart = () => {
                         </ul>
                     </div>
                     <div className={styles.productCards}>
-                        {cartProducts.map(product => (<CartItem key={product._id} product={product} removeFromCartHandler={removeFromCartHandler} />))}
+                        {cartProducts.map(product => (<CartItem key={product._id} product={product} productQuantity={quantity[product._id]} productQuantityHandler={productQuantityHandler} removeFromCartHandler={removeFromCartHandler} />))}
                     </div>
+                    <div>Grand Total Price:$ {grandTotalPrice}</div>
                 </>
             }
         </section>
