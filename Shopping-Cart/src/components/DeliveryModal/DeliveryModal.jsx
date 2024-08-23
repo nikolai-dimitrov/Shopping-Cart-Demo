9569924a315986662570c066d2b954bd16385c51
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-23 17:00:15 +0300

    Refactor: Add delivery form

--- Shopping-Cart/src/components/DeliveryModal/DeliveryModal.jsx
+++ Shopping-Cart/src/components/DeliveryModal/DeliveryModal.jsx
@@ -1,14 +1,11 @@
+import { DeliveryForm } from '../DeliveryForm/DeliveryForm'
 import styles from "./delivery-modal.module.css"
 export const DeliveryModal = ({ showModal, closeModalHandler }) => {
     return (
         <>
             <section className={showModal ? `${styles.delivery} ${styles.showDeliveryModal} ` : `${styles.delivery}`}>
-                <h1>Delivery Information</h1>
-                <form action="">
-                    <label htmlFor="">This is input</label>
-                    <input type="text" />
-                </form>
-                <button onClick={closeModalHandler}>Button</button>
+                <h1>Fill your shipping information</h1>
+                <DeliveryForm closeModalHandler={closeModalHandler} />
             </section>
         </>
     )
