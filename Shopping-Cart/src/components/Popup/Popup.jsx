56f92e1fb5226218488eb648c449c53c63281599
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-24 17:47:22 +0300

    feat: Create separate popup component

--- Shopping-Cart/src/components/Popup/Popup.jsx
+++ Shopping-Cart/src/components/Popup/Popup.jsx
@@ -1,6 +1,25 @@
+import { useState, useEffect } from 'react';
+import { notification } from 'antd';
+
 import styles from './popup.module.css'
-export const Popup = () => {
+export const Popup = ({ status, timesOpened, message, description }) => {
+    const [api, contextHolder] = notification.useNotification();
+
+    const showNotification = (status, message, description) => {
+        api[`${status}`]({
+            message: message,
+            description: description,
+            showProgress: true,
+            pauseOnHover: true,
+        })
+    }
+    useEffect(() => {
+        showNotification(status, message, description)
+    }, [timesOpened])
+
     return (
-        <>  </>
+        <>
+            {contextHolder}
+        </>
     )
 }
