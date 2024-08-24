1afbab0520c3d121da5956024b013c3f4ca3c9af
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-24 14:27:54 +0300

    refactor: Change and center modal title

--- Shopping-Cart/src/components/DeliveryModal/DeliveryModal.jsx
+++ Shopping-Cart/src/components/DeliveryModal/DeliveryModal.jsx
@@ -1,10 +1,11 @@
+import { useEffect } from 'react'
 import { DeliveryForm } from '../DeliveryForm/DeliveryForm'
 import styles from "./delivery-modal.module.css"
 export const DeliveryModal = ({ showModal, closeModalHandler }) => {
     return (
         <>
             <section className={showModal ? `${styles.delivery} ${styles.showDeliveryModal} ` : `${styles.delivery}`}>
-                <h1>Fill your shipping information</h1>
+                <h1>Finish purchase</h1>
                 <DeliveryForm closeModalHandler={closeModalHandler} />
             </section>
         </>
