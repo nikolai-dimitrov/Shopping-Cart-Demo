033d16f93d3a01c901de99d6c5c776689bbe6312
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-08-26 13:45:49 +0300

    refactor: Remove unused property

--- Shopping-Cart/src/hooks/usePopup.js
+++ Shopping-Cart/src/hooks/usePopup.js
@@ -3,19 +3,19 @@ import { useState } from "react";
 export const usePopup = () => {
 	const [popupState, setPopupState] = useState({
 		currentStatus: null,
-		open: false,
 		timesOpened: 0,
 		message: "",
 		description: "",
 	});
 
 	const showPopupHandler = (props) => {
+		console.log(props);
+
 		setPopupState((popupState) => ({
 			...popupState,
 			...props,
 			timesOpened: (popupState["timesOpened"] += 1),
 		}));
 	};
-	console.log(popupState, "state");
 	return [popupState, showPopupHandler];
 };
