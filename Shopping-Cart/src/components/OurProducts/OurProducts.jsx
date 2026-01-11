71cddb0cb64b399768c0473fb34f1e06afba9a2d
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2026-01-11 15:43:11 +0200

    feat: implement error handling for server error and retry functionality

--- Shopping-Cart/src/components/OurProducts/OurProducts.jsx
+++ Shopping-Cart/src/components/OurProducts/OurProducts.jsx
@@ -17,11 +17,14 @@ export const OurProducts = () => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [error, setError] = useState(null)
 	const [page, setPage] = useState(1);
+	const [retry, setRetry] = useState(0)
 
 	const [popupState, showPopupHandler] = usePopup();
 
 	useEffect(() => {
-		// setIsLoading(true)
+		setIsLoading(true);
+		setError(null)
+
 		fetch(`${import.meta.env.VITE_API_URL}/jsonstore/electronics`)
 			.then(response => response.json())
 			.then(data => {
@@ -30,35 +33,49 @@ export const OurProducts = () => {
 				setProducts(productsMatrix);
 				setIsLoading(false);
 			})
-			.catch(error => console.log(error))
-			// .finally(() => setIsLoading(false));
-	}, [])
+			.catch(error => setError(error))
+	}, [retry])
 
 	let totalProductsCount = products.reduce(
 		(count, currentArr) => count + currentArr.length,
 		0
 	);
 
+	const retryButtonHandler = () => {
+		setRetry((prev) => prev + 1)
+		setError(null)
+	}
+
 	return (
 		<>
 			<div className={styles.ourProducts}>
-				<Popup {...popupState} />
+				{error ?
+					<div>
+						<p>{error.message}</p>
+						<button onClick={retryButtonHandler}>Retry</button>
+					</div>
+					:
+					<>
+						<Popup {...popupState} />
 
-				{isLoading ? <CardSkeleton cards={9} /> : products[page - 1]?.map(item => (<Product key={item._id} {...item} showPopupHandler={showPopupHandler} />))}
-				<Pagination
-					simple={{
-						readOnly: true,
-					}}
-					defaultCurrent={1}
-					total={totalProductsCount}
-					current={page}
-					pageSize={9}
-					onChange={(page) => setPage(page)}
-					style={{
-						width: '93%',
-						justifyContent: 'center'
-					}}
-				/>
+						{isLoading ? <CardSkeleton cards={9} /> : products[page - 1]?.map(item => (<Product key={item._id} {...item} showPopupHandler={showPopupHandler} />))}
+						<Pagination
+							simple={{
+								readOnly: true,
+							}}
+							defaultCurrent={1}
+							total={totalProductsCount}
+							current={page}
+							pageSize={9}
+							onChange={(page) => setPage(page)}
+							style={{
+								width: '93%',
+								justifyContent: 'center'
+							}}
+						/>
+					</>
+
+				}
 			</div>
 		</>
 	)
